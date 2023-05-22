082d15b81b0eebe29fa64328535a526ccd4ce148
haoming9245 <haoming9245@outlook.com>
2023-05-22 22:43:07 +0800

    deleted

--- docs/.vuepress/.temp/pages/hant/quickstart/install.html.vue
+++
@@ -1,9 +0,0 @@
-<template><div><h1 id="安裝機器人" tabindex="-1"><a class="header-anchor" href="#安裝機器人" aria-hidden="true">#</a> 安裝機器人</h1>
-<h2 id="windows" tabindex="-1"><a class="header-anchor" href="#windows" aria-hidden="true">#</a> Windows</h2>
-<p>直接把儲存庫源代碼下載,解壓縮到一個文件夾</p>
-<h2 id="linux" tabindex="-1"><a class="header-anchor" href="#linux" aria-hidden="true">#</a> Linux</h2>
-<p>將源代碼clone下來(先<code v-pre>apt install git</code>或<code v-pre>yum install git</code>一下檢查有沒有安裝git)</p>
-<div class="language-text line-numbers-mode" data-ext="text"><pre v-pre class="language-text"><code>git clone https://github.com/QGAB-Team/qq-group-admin-bot
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div></template>
-
-
--- docs/.vuepress/.temp/pages/quickstart/install.html.vue
+++
@@ -1,9 +0,0 @@
-<template><div><h1 id="安装机器人" tabindex="-1"><a class="header-anchor" href="#安装机器人" aria-hidden="true">#</a> 安装机器人</h1>
-<h2 id="windows" tabindex="-1"><a class="header-anchor" href="#windows" aria-hidden="true">#</a> Windows</h2>
-<p>直接将存储库源码下载下来，解压到一个文件夹即可</p>
-<h2 id="linux" tabindex="-1"><a class="header-anchor" href="#linux" aria-hidden="true">#</a> Linux</h2>
-<p>将源码clone下来(先<code v-pre>apt install git</code>或<code v-pre>yum install git</code>一下看看有没有安装git)</p>
-<div class="language-text line-numbers-mode" data-ext="text"><pre v-pre class="language-text"><code>git clone https://github.com/QGAB-Team/qq-group-admin-bot
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></div></template>
-
-
